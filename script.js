477938124b8bb9cfe0a63b2169e4c959bc05a40a
familyhubus-art <familyhubus@gmail.com>
2026-01-30 22:21:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.getElementById("subscribeBtn").addEventListener("click", function () {
-  // Apna YouTube channel link yaha daalo
-  window.open("https://www.youtube.com/", "_blank");
+  window.open(
+    "https://www.youtube.com/@imrahulpathak/shorts",
+    "_blank"
+  );
 });
